e9a5ab461a7699bec6b672019a36d8141ec66922
Sumit Shetye <sshetye@ucsd.edu>
2024-02-28 10:49:27 -0800

    added triggers

--- testing/sketch.js
+++ testing/sketch.js
@@ -1,10 +1,33 @@
+var trig1;
+var trig2;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  trig1 = new Trigger(width * .2, height * .9, color(235,20,20));
+  trig1.display();
+
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
   //testing
 
   //testing merge
+}
+
+class Trigger {
+  constructor(xPos, yPos, color){
+    this.x = xPos;
+    this.y = yPos;
+    this.c = color
+  }
+
+  display(){
+    strokeWeight(4);
+    stroke(color);
+    circle(this.x, this.y, 50);
+  }
+
+  move(){
+
+  }
 }
